4fbcb63a6272512e8812a628f1f108fd739d60be
adeliya21 <adeliya.salieva@gmail.com>
2024-12-09 18:31:56 -0800

    added ieNoOpen

--- myApp.js
+++ myApp.js
@@ -5,7 +5,7 @@ app.use(helmet.hidePoweredBy()); // This hides the tech used to build web - Expr
 app.use(helmet.frameguard({action: 'deny'})); // This sets X-Frame-Option header set to deny - against click jacking.
 app.use(helmet.xssFilter()); // This sanitizes input sent to your server - against Cross-Site Scripting attacks.
 app.use(helmet.noSniff()); // This sets the X-Content-Type-Options header to nosniff, instructing the browser to not bypass the provided Content-Type - aginst MIME/content sniffing attacks.
-
+app.use(helmet.ieNoOpen()); // This prevents Internet Explorer from executing HTML downloads (by default) in context of your trusted site.
 
 
 
