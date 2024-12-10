946457bc844bd6e314e466ef3ff7ca1b0f1138ed
adeliya21 <adeliya.salieva@gmail.com>
2024-12-09 18:26:07 -0800

    added noSniff

--- myApp.js
+++ myApp.js
@@ -4,6 +4,7 @@ const app = express();
 app.use(helmet.hidePoweredBy()); // This hides the tech used to build web - Express.
 app.use(helmet.frameguard({action: 'deny'})); // This sets X-Frame-Option header set to deny - against click jacking.
 app.use(helmet.xssFilter()); // This sanitizes input sent to your server - against Cross-Site Scripting attacks.
+app.use(helmet.noSniff()); // This sets the X-Content-Type-Options header to nosniff, instructing the browser to not bypass the provided Content-Type - aginst MIME/content sniffing attacks.
 
 
 
